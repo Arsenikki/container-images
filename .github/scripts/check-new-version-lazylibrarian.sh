1d9051b65f54106fe3e2006acc5c9d04b6330a89
Arsenikki <arsenileskinen@gmail.com>
2022-04-23 09:22:03 +0300

    Get image labels directly from DockerHub without pulling

--- .github/scripts/check-new-version-lazylibrarian.sh
+++ .github/scripts/check-new-version-lazylibrarian.sh
@@ -1,2 +1,13 @@
-docker image inspect --format '{{json .}}' linuxserver/lazylibrarian:latest | 
-  jq -r '.Config.Labels."org.opencontainers.image.version"'
+#!/bin/bash
+TEMPFILE=$1
+
+REPO=linuxserver/lazylibrarian                                                                                                                                                                          
+
+TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${REPO}:pull" \
+  | jq -r '.token')
+
+VERSION=`curl -s -H "Authorization: Bearer $TOKEN" "https://registry-1.docker.io/v2/${REPO}/manifests/latest" \
+  | jq ".history[0].v1Compatibility" -r | jq '.config.Labels."org.opencontainers.image.version"'`
+
+echo $VERSION > $TEMPFILE
+echo "Created $TEMPFILE file with tag: $VERSION"
