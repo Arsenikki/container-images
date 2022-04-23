94b6335b5ba8ac746c4d3ac82e44421fd4537147
Arsenikki <arsenileskinen@gmail.com>
2022-04-23 09:47:48 +0300

    remove quotes from jq

--- .github/scripts/check-new-version-lazylibrarian.sh
+++ .github/scripts/check-new-version-lazylibrarian.sh
@@ -7,7 +7,7 @@ TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=r
   | jq -r '.token')
 
 VERSION=`curl -s -H "Authorization: Bearer $TOKEN" "https://registry-1.docker.io/v2/${REPO}/manifests/latest" \
-  | jq ".history[0].v1Compatibility" -r | jq '.config.Labels."org.opencontainers.image.version"'`
+  | jq ".history[0].v1Compatibility" -r | jq -r '.config.Labels."org.opencontainers.image.version"'`
 
 echo $VERSION > $TEMPFILE
 echo "Created $TEMPFILE file with tag: $VERSION"
